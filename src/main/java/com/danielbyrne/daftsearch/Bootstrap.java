6d2d80fd30d348b1656083e4868b55bbbf8c31da
Daniel Byrne <byrne.dan01@gmail.com>
2019-08-14 16:45:45 +0100

    Getting links for target page.

--- src/main/java/com/danielbyrne/daftsearch/Bootstrap.java
+++ src/main/java/com/danielbyrne/daftsearch/Bootstrap.java
@@ -7,19 +7,26 @@ import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 public class Bootstrap {
 
     public Bootstrap() throws IOException {
 
-        Document document = Jsoup.connect("http://en.wikipedia.org/").get();
+        Document document = Jsoup.connect("https://www.daft.ie/wicklow/property-for-sale/").get();
+        Elements newsHeadlines = document.select(".PropertyCardContainer__container > a, .PropertyImage__mainImageContainerStandard > a");
 
-        System.out.println(document.title());
-        Elements newsHeadlines = document.select("#mp-itn b a");
+        Set<String> links = new HashSet<>();
 
         for (Element headline : newsHeadlines) {
-            System.out.println(headline.attr("title") + ": " + headline.absUrl("href"));
+            links.add(headline.absUrl("href"));
+        }
+
+        System.out.println(links.size());
+        for (String s : links) {
+            System.out.println(s);
         }
     }
 }
