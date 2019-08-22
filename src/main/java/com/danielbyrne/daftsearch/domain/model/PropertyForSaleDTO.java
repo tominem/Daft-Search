3816057a941ad9d4f1d07192380a23634dfc6786
Daniel Byrne <byrne.dan01@gmail.com>
2019-08-22 10:25:16 +0100

    Added Sale DTO. Closes #15

---
+++ src/main/java/com/danielbyrne/daftsearch/domain/model/PropertyForSaleDTO.java
@@ -0,0 +1,24 @@
+package com.danielbyrne.daftsearch.domain.model;
+
+import com.danielbyrne.daftsearch.domain.County;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PropertyForSaleDTO {
+
+    private String address;
+    private String eircode;
+    private int beds;
+    private int baths;
+    private String propertyType;
+    private String priceString;
+    private String link;
+    private County county;
+
+    private String readableDistance;
+    private String readableDuration;
+
+    private String description;
+}
