e2ac316bf8fb77aedbecafdf575228970978cbfb
Daniel Byrne <byrne.dan01@gmail.com>
2019-08-22 10:30:08 +0100

    Added Let DTO. Closes #16

---
+++ src/main/java/com/danielbyrne/daftsearch/domain/model/PropertyForRentDTO.java
@@ -0,0 +1,27 @@
+package com.danielbyrne.daftsearch.domain.model;
+
+import com.danielbyrne.daftsearch.domain.County;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PropertyForRentDTO {
+
+    private String address;
+    private String eircode;
+    private int beds;
+    private int baths;
+    private String propertyType;
+    private String priceString;
+    private String link;
+    private County county;
+
+    private String readableDistance;
+    private String readableDuration;
+
+    private String description;
+
+    private String moveInDate;
+    private String leaseLength;
+}
