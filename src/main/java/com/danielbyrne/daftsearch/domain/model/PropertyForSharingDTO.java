76a884e2ed1b828ca27a9f1e7d5b4d111d043c1b
Daniel Byrne <byrne.dan01@gmail.com>
2019-08-22 10:32:59 +0100

    Added Sharing DTO. Close #17

---
+++ src/main/java/com/danielbyrne/daftsearch/domain/model/PropertyForSharingDTO.java
@@ -0,0 +1,31 @@
+package com.danielbyrne.daftsearch.domain.model;
+
+import com.danielbyrne.daftsearch.domain.County;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PropertyForSharingDTO {
+
+    private String address;
+    private String eircode;
+    private int beds;
+    private int baths;
+    private String propertyType;
+    private String priceString;
+    private String link;
+    private County county;
+
+    private String readableDistance;
+    private String readableDuration;
+
+    private String description;
+
+    private boolean malesOnly;
+    private boolean femalesOnly;
+    private boolean ownerOccupied;
+    private int currentOccupants;
+    private boolean hasSingle;
+    private boolean hasDouble;
+}
