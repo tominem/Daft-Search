856befb705a48bb59c72c810eae01ed12dc0f9cb
Daniel Byrne <byrne.dan01@gmail.com>
2019-08-22 10:47:55 +0100

    Forgot Mapper annotation

--- src/main/java/com/danielbyrne/daftsearch/domain/mappers/PropertyForSharingMapper.java
+++ src/main/java/com/danielbyrne/daftsearch/domain/mappers/PropertyForSharingMapper.java
@@ -2,8 +2,10 @@ package com.danielbyrne.daftsearch.domain.mappers;
 
 import com.danielbyrne.daftsearch.domain.PropertyForSharing;
 import com.danielbyrne.daftsearch.domain.model.PropertyForSharingDTO;
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+@Mapper
 public interface PropertyForSharingMapper {
 
     PropertyForSharingMapper INSTANCE = Mappers.getMapper(PropertyForSharingMapper.class);
