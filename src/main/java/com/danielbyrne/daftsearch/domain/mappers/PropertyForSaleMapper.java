44ac47e5a2e129e392d02587a04daef9dd4f150b
Daniel <byrne.dan01@gmail.com>
2019-08-22 12:18:57 +0100

    Wrong type

--- src/main/java/com/danielbyrne/daftsearch/domain/mappers/PropertyForSaleMapper.java
+++ src/main/java/com/danielbyrne/daftsearch/domain/mappers/PropertyForSaleMapper.java
@@ -1,7 +1,6 @@
 package com.danielbyrne.daftsearch.domain.mappers;
 
 import com.danielbyrne.daftsearch.domain.PropertyForSale;
-import com.danielbyrne.daftsearch.domain.model.PropertyForRentDTO;
 import com.danielbyrne.daftsearch.domain.model.PropertyForSaleDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
@@ -11,7 +10,7 @@ public interface PropertyForSaleMapper {
 
     PropertyForSaleMapper INSTANCE = Mappers.getMapper(PropertyForSaleMapper.class);
 
-    PropertyForRentDTO propertyForSaleToPropertyForSaleDTO(PropertyForSale propertyForSale);
+    PropertyForSaleDTO propertyForSaleToPropertyForSaleDTO(PropertyForSale propertyForSale);
 
     PropertyForSale propertyForSaleDTOtoPropertyForSale(PropertyForSaleDTO propertyForSaleDTO);
 }
