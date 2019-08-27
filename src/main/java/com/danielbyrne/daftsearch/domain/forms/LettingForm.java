40ae45593eaecd550f7cfa4f7c45565bca2d3741
Daniel Byrne <byrne.dan01@gmail.com>
2019-08-27 14:21:06 +0100

    Leveraging inheritance so as not to declare same constraints across multiple classes

--- src/main/java/com/danielbyrne/daftsearch/domain/forms/LettingForm.java
+++ src/main/java/com/danielbyrne/daftsearch/domain/forms/LettingForm.java
@@ -1,33 +1,10 @@
 package com.danielbyrne.daftsearch.domain.forms;
 
-import com.danielbyrne.daftsearch.domain.County;
-import com.danielbyrne.daftsearch.domain.ModeOfTransport;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 @Getter
 @Setter
-public class LettingForm {
+public class LettingForm extends SaleForm {
 
-    @NotNull
-    private int minBeds;
-
-    @NotNull
-    private float maxPrice;
-
-    @NotNull
-    private int distanceInKms;
-
-    @NotNull
-    private String location;
-
-    @NotNull
-    @Size(min = 1, max = 5)
-    private County[] counties;
-
-    private int commuteInMinutes;
-    private ModeOfTransport modeOfTransport;
 }
--- src/main/java/com/danielbyrne/daftsearch/domain/forms/SharingForm.java
+++ src/main/java/com/danielbyrne/daftsearch/domain/forms/SharingForm.java
@@ -1,40 +1,14 @@
 package com.danielbyrne.daftsearch.domain.forms;
 
-import com.danielbyrne.daftsearch.domain.County;
-import com.danielbyrne.daftsearch.domain.ModeOfTransport;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 @Getter
 @Setter
-public class SharingForm {
-
-    @NotNull
-    @Min(1)
-    private float maxPrice;
-
-    @NotNull
-    @Min(1)
-    private int distanceInKms;
-
-    private int commuteInMinutes;
-
-    @NotNull
-    private String location;
-
-    @NotNull
-    @Size(min = 1, max = 5)
-    private County[] counties;
+public class SharingForm extends LettingForm {
 
     private String roomType;
-
-    private ModeOfTransport modeOfTransport;
     private boolean male;
-
     private boolean hasSingle;
     private boolean hasDouble;
     private boolean hasShared;
