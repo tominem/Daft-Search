37a4f2d173e08463852bf9e628c9631b8e9af8d8
Daniel Byrne <byrne.dan01@gmail.com>
2019-08-27 14:03:24 +0100

    Added extra form validation

--- src/main/java/com/danielbyrne/daftsearch/domain/forms/SaleForm.java
+++ src/main/java/com/danielbyrne/daftsearch/domain/forms/SaleForm.java
@@ -5,19 +5,25 @@ import com.danielbyrne.daftsearch.domain.ModeOfTransport;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
 public class SaleForm {
 
+    @Positive
     @NotNull
     private Integer minBeds;
 
+    @Positive
     @NotNull
     private Float maxPrice;
 
-    @PositiveOrZero
+    @Positive
+    @NotNull
     private Integer distanceInKms;
 
     @NotBlank
@@ -27,7 +33,8 @@ public class SaleForm {
     @Size(min = 1, max = 5)
     private County[] counties;
 
-    @Min(0)
+    @Positive
+    @NotNull
     private Integer commuteInMinutes;
 
     @NotNull
